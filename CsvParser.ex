0de1e7fa49553e3cd41a543db628f97f52bafca4
Dmitry Yudin <yudindm@mail.ru>
2015-11-28 13:44:27 +0300

    Разбор csv по одной строке за раз

--- CsvParser.ex
+++ CsvParser.ex
@@ -9,34 +9,67 @@ defmodule CsvParser do
   def parse(nil, result) do
     result
   end
+
+  def parse({"\"", rest}, {:start, [_ | t]}) do
+    parse(String.next_codepoint(rest), {:quot, ["" | t]})
+  end
+  def parse({",", rest}, {:start, data}) do
+    parse(String.next_codepoint(rest), {:start, push_field(data)})
+  end
+  def parse({" ", rest}, {:start, data}) do
+    parse(String.next_codepoint(rest), {:start, push_char(" ", data)})
+  end
+  def parse({"\n", rest}, {:start, data}) do
+    {:ok, :lists.reverse(data), rest}
+  end
+  def parse({c, rest}, {:start, data}) do
+    parse(String.next_codepoint(rest), {:norm, push_char(c, data)})
+  end
+
   def parse({"\"", rest}, {:quot, data}) do
     parse(String.next_codepoint(rest), {:qquot, data})
   end
   def parse({c, rest}, {:quot, data}) do
     parse(String.next_codepoint(rest), {:quot, push_char(c, data)})
   end
+
   def parse({"\"", rest}, {:qquot, data}) do
     parse(String.next_codepoint(rest), {:quot, push_char("\"", data)})
   end
   def parse({c, rest}, {:qquot, data}) do
-    parse({c, rest}, {:end, data})
+    parse({c, rest}, {:endquot, data})
   end
-  def parse({"\n", rest}, {_, data}) do
+
+  def parse({" ", rest}, {:endquot, data}) do
+    parse(String.next_codepoint(rest), {:endquot, data})
+  end
+  def parse({",", rest}, {:endquot, data}) do
+    parse(String.next_codepoint(rest), {:start, push_field(data)})
+  end
+  def parse({"\n", rest}, {:endquot, data}) do
     {:ok, :lists.reverse(data), rest}
   end
-  def parse({",", rest}, {:part, data}) do
-    parse(String.next_codepoint(rest), push_field(data))
+  def parse({c, _}, {:endquot, _}) do
+    {:error, :invalid_char, c}
   end
-  def parse({"\"", rest}, {:part, data}) do
-    parse(String.next_codepoint(rest), {:quot, data})
+
+  def parse({"\n", rest}, {:norm, data}) do
+    {:ok, :lists.reverse(data), rest}
   end
-  def parse({c, rest}, {status, data}) do
-    parse(String.next_codepoint(rest), {status, push_char(c, data)})
+  def parse({",", rest}, {:norm, data}) do
+    parse(String.next_codepoint(rest), {:norm, push_field(data)})
   end
+  def parse({"\"", rest}, {:norm, data}) do
+    {:error, :invalid_char, "\""}
+  end
+  def parse({c, rest}, {:norm, data}) do
+    parse(String.next_codepoint(rest), {:norm, push_char(c, data)})
+  end
+
   def push_char(c, [fld | flds]) do
     [fld <> c | flds]
   end
   def push_field(data) do
-    {:part, ["" | data] }
+    ["" | data]
   end
 end
