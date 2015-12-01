8dfa62e112f80d3874eefd84f65fcb6e69502f42
Dmitry Yudin <yudindm@mail.ru>
2015-12-01 22:21:23 +0300

    Наброски вызова трансформации в CsvStream

--- CsvStream.ex
+++ CsvStream.ex
@@ -1,5 +1,13 @@
 defmodule CsvStream do
   def as_csv(e) do
-    Stream.transform(
+    Stream.transform(e,
+      fn -> {:start, ""} end,
+      &parse_next/2,
+      &finalize/1)
+  end
+  defp parse_next(chunk, acc) do
+    case CsvParser.parse(chunk, acc) do
+      {:ok, record, rest} -> 
+    end
   end
 end
