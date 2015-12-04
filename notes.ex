72f4e3d6e9360449bac71b5e35ddb58511a37b0b
Dmitry Yudin <yudindm@mail.ru>
2015-12-04 22:11:27 +0300

    Чтение заметок из файла csv

--- notes.ex
+++ notes.ex
@@ -1,13 +1,32 @@
 defmodule Notes do
   defmodule Date do
     defstruct day: 0, mon: 0, year: 0
+
+    def parse(str) do
+      [day, mon, year] = String.split(str, ".")
+      %Date{day: day, mon: mon, year: year}
+    end
   end
   defmodule Rec do
     defstruct text: nil, date: %Date{}, flag: :none
+
+    def create([text, date, flag]) do
+      %Rec{text: text, date: Date.parse(date), flag: parse_flag(flag)}
+    end
+    defp parse_flag(str) do
+      case str do
+        "" -> :none
+        "важная" -> :important
+        "особая" -> :urgent
+        "обычная" -> :normal
+        "низкая" -> :notimportant
+      end
+    end
   end
   def read(filename) do
     File.stream!(filename) |>
-    Stream.map(&(&1)) |>
+    CsvStream.as_csv() |>
+    Stream.map(&(Rec.create(&1))) |>
     Enum.to_list()
   end
 end
